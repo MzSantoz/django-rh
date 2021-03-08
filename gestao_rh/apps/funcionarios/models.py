a33dbc17e2210f7f01ba0bfde40957b65ddf4ca9
Moises Santos <mzsantoz@outlook.com>
2021-03-08 11:49:22 -0300

    Creating Funcionarios App

--- gestao_rh/apps/funcionarios/models.py
+++ gestao_rh/apps/funcionarios/models.py
@@ -1,11 +1,11 @@
 import uuid
 from django.db import models
 
-
-class Empresa(models.Model):
+class Funcionario(models.Model):
   id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-  nome = models.CharField(max_length=100, help_text="Nome da empresa")
+  nome = models.CharField(max_length=100)
 
   def __str__(self):
       return self.nome
+  
 
